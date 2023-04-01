3fd024d25017725001388ce33f60576c28adbbb5
Robert Vincic <robert.vincic@gmail.com>
2023-04-01 20:00:06 +0200

    sample database test

---
+++ eOdvjetnik/src/GeneratedCrudCode.cs
@@ -0,0 +1,46 @@
+
+        public class Sample
+        {
+            public tinyint active { get; set; }
+public int id { get; set; }
+public varchar name { get; set; }
+        }
+        
+        public class SampleService
+        {
+            
+        public async Task<List<Sample>>> GetAllSamples()
+        {
+            // TODO: Implement code to fetch all Samples from 
+database
+            throw new NotImplementedException();
+        }
+        
+        public async Task<Sample> GetSample(int id)
+        {
+            // TODO: Implement code to fetch Sample with the given 
+ID from database
+            throw new NotImplementedException();
+        }
+        
+        public async Task AddSample(Sample sample)
+        {
+            // TODO: Implement code to insert Sample into database
+            throw new NotImplementedException();
+        }
+        
+        public async Task UpdateSample(Sample sample)
+        {
+            // TODO: Implement code to update Sample in database
+            throw new NotImplementedException();
+        }
+        
+        public async Task DeleteSample(int id)
+        {
+            // TODO: Implement code to delete Sample with the given 
+ID from database
+            throw new NotImplementedException();
+        }
+    
+        }
+    
