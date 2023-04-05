d507af3322f4774e68adffe8b093ba05e352f131
Stojakovix <121807703+Stojakovix@users.noreply.github.com>
2023-04-05 16:58:55 +0200

    Hehe baci oko

---
+++ eOdvjetnik/ViewModel/KalendarViewModel.cs
@@ -0,0 +1,54 @@
+﻿
+using Syncfusion.Maui.Scheduler;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace eOdvjetnik.ViewModel
+{
+    public class KalendarViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<SchedulerAppointment> appointments;
+
+        public KalendarViewModel()
+        {
+            Appointments = new ObservableCollection<SchedulerAppointment>
+            {
+                //Adding scheduler appointment in the scheduler appointment collection. 
+                new SchedulerAppointment()
+                {
+                    StartTime = DateTime.Today.AddHours(9),
+                    EndTime = DateTime.Today.AddHours(11),
+                    Subject = "Client Meeting",
+                    Location = "Hutchison road",
+                }
+            };
+        }
+
+        /// <summary>
+        /// Property changed event handler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<SchedulerAppointment> Appointments
+        {
+            get
+            {
+                return appointments;
+            }
+            set
+            {
+                appointments = value;
+                this.RaiseOnPropertyChanged(nameof(Appointments));
+            }
+        }
+
+        /// <summary>
+        /// Invoke method when property changed.
+        /// </summary>
+        /// <param name="propertyName">property name</param>
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- eOdvjetnik/Views/AppointmentDialog.xaml.cs
+++ eOdvjetnik/Views/AppointmentDialog.xaml.cs
@@ -1,50 +1,154 @@
-using eOdvjetnik.Models;
 using Syncfusion.Maui.Scheduler;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-
 
 namespace eOdvjetnik.Views;
 
 public partial class AppointmentDialog : ContentPage
 {
-    public ObservableCollection<SchedulerAppointment> Appointments { get; set; } = new();
-    public Meeting Meeting { get; set; } = new Meeting();
-    public AppointmentDialog()
+    SchedulerAppointment appointment;
+    DateTime selectedDate;
+    SfScheduler scheduler;
+
+    public AppointmentDialog(SchedulerAppointment appointment, DateTime selectedDate, SfScheduler scheduler)
     {
         InitializeComponent();
-        BindingContext = Meeting;
+        this.appointment = appointment;
+        this.selectedDate = selectedDate;
+        this.scheduler = scheduler;
+        cancelButton.BackgroundColor = Color.FromArgb("#E5E5E5");
+        saveButton.BackgroundColor = Color.FromArgb("#2196F3");
+        eventNameText.Placeholder = "Event name";
+        organizerText.Placeholder = "update the notes";
+
+        UpdateEditor();
+        saveButton.Clicked += SaveButton_Clicked;
+        cancelButton.Clicked += CancelButton_Clicked;
+        switchAllDay.Toggled += SwitchAllDay_Toggled;
+        DeleteButton.Clicked += DeleteButton_Clicked;
+
     }
 
-    private void Save_Clicked(object sender, EventArgs e)
+    private void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        //Debug.WriteLine($"Meeting EventName: {Meeting.EventName}");
-        //Debug.WriteLine($"Meeting to: {Meeting.From:f}");
-        //Debug.WriteLine($"Meeting EndTime: {Meeting.To:f}");
-        
+        (this.scheduler.AppointmentsSource as ObservableCollection<SchedulerAppointment>).Remove(this.appointment);
+        this.Navigation.PopAsync();
+    }
 
-        var appointment = new SchedulerAppointment()
+    private void SwitchAllDay_Toggled(object sender, ToggledEventArgs e)
+    {
+        if ((sender as Switch).IsToggled)
         {
-            Subject = Meeting.EventName,
-            StartTime = Meeting.From, 
-            EndTime = Meeting.To,
-            
-            
-        };
-        
-
-        Appointments.Add(appointment);
-        Meeting = new();
-        BindingContext = Meeting;
-
-
-        Navigation.PopAsync();
-
-        Debug.WriteLine("svi eventi u oc u ");
-        foreach (SchedulerAppointment app in Appointments)
+            startTime_picker.Time = new TimeSpan(12, 0, 0);
+            startTime_picker.IsEnabled = false;
+            endTime_picker.Time = new TimeSpan(12, 0, 0);
+            endTime_picker.IsEnabled = false;
+        }
+        else
         {
-            Debug.WriteLine($"Subject: {app.Subject}, Start Time: {app.StartTime}, End Time: {app.EndTime}");
+            startTime_picker.IsEnabled = true;
+            endTime_picker.IsEnabled = true;
+            (sender as Switch).IsToggled = false;
+        }
+    }
+    private void CancelButton_Clicked(object sender, EventArgs e)
+    {
+        this.Navigation.PopAsync();
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        var endDate = endDate_picker.Date;
+        var startDate = startDate_picker.Date;
+        var endTime = endTime_picker.Time;
+        var startTime = startTime_picker.Time;
+
+        if (endDate < startDate)
+        {
+            Application.Current.MainPage.DisplayAlert("", "End time should be greater than start time", "OK");
+        }
+        else if (endDate == startDate)
+        {
+            if (endTime < startTime)
+            {
+                Application.Current.MainPage.DisplayAlert("", "End time should be greater than start time", "OK");
+            }
+            else
+            {
+                AppointmentDetails();
+                this.IsVisible = false;
+            }
+        }
+        else
+        {
+            AppointmentDetails();
+            this.IsVisible = false;
+        }
+    }
+
+    private void AppointmentDetails()
+    {
+        if (appointment == null)
+        {
+            var scheduleAppointment = new SchedulerAppointment();
+            scheduleAppointment.Subject = this.eventNameText.Text;
+            scheduleAppointment.StartTime = this.startDate_picker.Date.Add(this.startTime_picker.Time);
+            scheduleAppointment.EndTime = this.endDate_picker.Date.Add(this.endTime_picker.Time);
+            scheduleAppointment.IsAllDay = this.switchAllDay.IsToggled;
+            scheduleAppointment.Notes = this.organizerText.Text;
+
+
+            if (this.scheduler.AppointmentsSource == null)
+            {
+                this.scheduler.AppointmentsSource = new ObservableCollection<SchedulerAppointment>();
+            }
+
+           (this.scheduler.AppointmentsSource as ObservableCollection<SchedulerAppointment>).Add(scheduleAppointment);
+        }
+        else
+        {
+            appointment.Subject = this.eventNameText.Text;
+            appointment.StartTime = this.startDate_picker.Date.Add(this.startTime_picker.Time);
+            appointment.EndTime = this.endDate_picker.Date.Add(this.endTime_picker.Time);
+            appointment.IsAllDay = this.switchAllDay.IsToggled;
+            appointment.Notes = this.organizerText.Text;
+        }
+
+        this.Navigation.PopAsync();
+    }
+
+    private void UpdateEditor()
+    {
+        if (appointment != null)
+        {
+            eventNameText.Text = appointment.Subject.ToString();
+            organizerText.Text = appointment.Notes;
+            startDate_picker.Date = appointment.StartTime;
+            endDate_picker.Date = appointment.EndTime;
+
+            if (!appointment.IsAllDay)
+            {
+                startTime_picker.Time = new TimeSpan(appointment.StartTime.Hour, appointment.StartTime.Minute, appointment.StartTime.Second);
+                endTime_picker.Time = new TimeSpan(appointment.EndTime.Hour, appointment.EndTime.Minute, appointment.EndTime.Second);
+                switchAllDay.IsToggled = false;
+            }
+            else
+            {
+                startTime_picker.Time = new TimeSpan(12, 0, 0);
+                startTime_picker.IsEnabled = false;
+                endTime_picker.Time = new TimeSpan(12, 0, 0);
+                endTime_picker.IsEnabled = false;
+                switchAllDay.IsToggled = true;
+            }
+        }
+        else
+        {
+            eventNameText.Text = "";
+            organizerText.Text = "";
+            switchAllDay.IsToggled = false;
+            startDate_picker.Date = selectedDate;
+            startTime_picker.Time = new TimeSpan(selectedDate.Hour, selectedDate.Minute, selectedDate.Second);
+            endDate_picker.Date = selectedDate;
+            endTime_picker.Time = new TimeSpan(selectedDate.Hour + 1, selectedDate.Minute, selectedDate.Second);
         }
     }
 }
-////No bugs found.
