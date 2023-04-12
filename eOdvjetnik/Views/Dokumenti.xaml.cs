6328092ef5599c1f1c400c0c694c919e6f8a4ff2
Stojakovix <121807703+Stojakovix@users.noreply.github.com>
2023-04-12 10:30:45 +0200

    ne radi prikaz ikona

--- eOdvjetnik/Views/Dokumenti.xaml.cs
+++ eOdvjetnik/Views/Dokumenti.xaml.cs
@@ -6,7 +6,7 @@ using SMBLibrary.Client;
 using System;
 using System.Reflection;
 using SMBLibrary.SMB1;
-
+using Microsoft.Maui.Controls;
 
 namespace eOdvjetnik.Views;
 
@@ -63,7 +63,20 @@ public partial class Dokumenti : ContentPage
 
                         }
 
-                        status = fileStore.CloseFile(directoryHandle);
+                    var listView = new ListView
+                    {
+                        ItemsSource = ShareFiles
+                    };
+
+                    listView.ItemTemplate = new DataTemplate(() =>
+                    {
+                        var imageCell = new ImageCell();
+                        imageCell.SetBinding(ImageCell.TextProperty, ".");
+                        imageCell.SetBinding(ImageCell.ImageSourceProperty, new Binding("Resources/icons/folder_1484.png", BindingMode.Default, null, null, null, new FileImageSourceConverter()));
+                        return imageCell;
+                    });
+
+                status = fileStore.CloseFile(directoryHandle);
                         foreach (var file1 in fileList)
                         {
                             System.Diagnostics.Debug.WriteLine(file1.Length.ToString());
