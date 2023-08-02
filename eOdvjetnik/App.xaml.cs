b829c0d73be2aa75cfdcf3efcd727d5becfb623d
Stojakovix <121807703+Stojakovix@users.noreply.github.com>
2023-08-02 19:00:33 +0200

    radi prijevod bre

--- eOdvjetnik/App.xaml.cs
+++ eOdvjetnik/App.xaml.cs
@@ -21,6 +21,12 @@ public partial class App : Application
         InitializeComponent();
         Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjQ5MTQ1M0AzMjMyMmUzMDJlMzBRT2JkTm1HczFuTmdmNTVFcWNWU29xbGt6Z2lhRDFYYk1GZWppS3pjWnlNPQ==");
 
+        //SfSchedulerResources.ResourceManager = new ResourceManager("eOdvjetnik.Resources.SfScheduler", Application.Current.GetType().Assembly);
+        SfSchedulerResources.ResourceManager = new ResourceManager("eOdvjetnik.Resources.SfScheduler.sr-SR", Application.Current.GetType().Assembly);
+        //CultureInfo.CurrentCulture = new CultureInfo("hr-HR");
+        //CultureInfo.CurrentUICulture = new CultureInfo("hr-HR");
+        CultureInfo.CurrentCulture = new CultureInfo("sr-SR");
+        CultureInfo.CurrentUICulture = new CultureInfo("sr-SR");
 
         Application.Current.UserAppTheme = AppTheme.Light;
         MainPage = new AppShell();
