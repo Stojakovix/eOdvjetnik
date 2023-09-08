90636f4a81bb8fdd2c7a951d4487b99684dd4b9e
Robert Vinčić <robert.vincic@gmail.com>
2023-09-08 16:10:13 +0200

    Fix 2 dokumenti

--- eOdvjetnik/ViewModel/DocsViewModel.cs
+++ eOdvjetnik/ViewModel/DocsViewModel.cs
@@ -234,7 +234,7 @@ namespace eOdvjetnik.ViewModel
                 //var fileUri = new Uri($"smb://{Preferences.Get(IP, "")}/{Preferences.Get(FOLDER, "")}/{fileName}");
                 //await Launcher.OpenAsync(fileUri);
                 Debug.WriteLine("Samo string -> "+@"\\192.168.1.211\Users\user\test.doc");
-                string filePath = @"\\" + Preferences.Get(IP, "") + "\\" + Preferences.Get(FOLDER_nas, "") + Preferences.Get(SUBFOLDER_nas, "") + "\\" + fileName;
+                string filePath = @"\\" + Preferences.Get(IP, "") + "\\" + Preferences.Get(FOLDER_nas, "")+ "\\" + Preferences.Get(SUBFOLDER_nas, "") + "\\" + fileName;
                 Debug.WriteLine("Izgenerirani string -> " + @"\\" + Preferences.Get(IP, "") + "\\" + Preferences.Get(FOLDER_nas, "") +"\\"+ Preferences.Get(SUBFOLDER_nas, "") + "\\" + fileName);
                 await Launcher.OpenAsync(filePath);
 
