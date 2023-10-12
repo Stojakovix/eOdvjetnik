95155f4c2c0102d4210a4e81a96891f1299c1528
Robert Vinčić <robert.vincic@gmail.com>
2023-10-12 07:36:54 +0200

    Kodiranje za Brunu i Syncfusion update

--- eOdvjetnik/Services/TrecaSreca.cs
+++ eOdvjetnik/Services/TrecaSreca.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace eOdvjetnik.Services
 {
@@ -106,6 +108,104 @@ namespace eOdvjetnik.Services
             XDocument xmlDoc = XDocument.Load(FilePath);
             xmlDoc.Root.Elements().Remove();
             xmlDoc.Save(FilePath);
+        }
+
+
+        private static byte[] GenerateRandomKey()
+        {
+            using var aes = Aes.Create();
+            aes.GenerateKey();
+            return aes.Key;
+        }
+
+        private static byte[] GenerateRandomIV()
+        {
+            using var aes = Aes.Create();
+            aes.GenerateIV();
+            return aes.IV;
+        }
+
+
+
+        public static void init()
+        {
+            string original = "Hello, world!";
+            byte[] key = GenerateRandomKey();
+            byte[] iv = GenerateRandomIV();
+
+
+
+
+
+
+
+
+            Set("license", Convert.ToBase64String(key));
+            Set("activation_key", Convert.ToBase64String(iv));
+
+
+            byte[] key2 = Convert.FromBase64String(Get("license"));
+            byte[] iv2 = Convert.FromBase64String(Get("activation_key"));
+
+            /*
+             * 
+             * 
+        sb.AppendLine($"Model: {Microsoft.Maui.Devices.DeviceInfo.Current.Model}");
+        sb.AppendLine($"Manufacturer: {Microsoft.Maui.Devices.DeviceInfo.Current.Manufacturer}");
+        sb.AppendLine($"Name: {Microsoft.Maui.Devices.DeviceInfo.Current.Name}");
+        sb.AppendLine($"OS Version: {Microsoft.Maui.Devices.DeviceInfo.Current.VersionString}");
+        sb.AppendLine($"Idiom: {Microsoft.Maui.Devices.DeviceInfo.Current.Idiom}");
+        sb.AppendLine($"Platform: {Microsoft.Maui.Devices.DeviceInfo.Current.Platform}");
+
+        Model: X570 AORUS ELITE
+        Manufacturer: Gigabyte Technology Co., Ltd.
+        Name: DESKTOP-NGMDCMT
+        OS Version: 10.0.19045.3570
+        Idiom: Desktop
+        Platform: WinUI
+             *
+             *
+             */
+
+
+
+
+
+
+
+            //Primjer ispod
+            byte[] encrypted = EncryptString(original, key, iv);
+            string decrypted = DecryptString(encrypted, key, iv);
+
+            Debug.WriteLine("************************************************************");
+            Debug.WriteLine($"Original: {original}");
+            Debug.WriteLine($"Encrypted b64: {Convert.ToBase64String(encrypted)}");
+            Debug.WriteLine($"Decrypted: {decrypted}");
+            Debug.WriteLine("************************************************************");
+        }
+
+
+        private static byte[] EncryptString(string plaintext, byte[] key, byte[] iv){
+            using var aes = Aes.Create();
+            aes.Key = key;
+            aes.IV = iv;
+
+            var encryptor = aes.CreateEncryptor();
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(plaintextBytes, 0, plaintextBytes.Length);
+            return encryptedBytes;
+        }
+
+        private static string DecryptString(byte[] ciphertext, byte[] key, byte[] iv)
+        {
+            using var aes = Aes.Create();
+            aes.Key = key;
+            aes.IV = iv;
+
+            var decryptor = aes.CreateDecryptor();
+            byte[] decryptedBytes = decryptor.TransformFinalBlock(ciphertext, 0, ciphertext.Length);
+            string plaintext = Encoding.UTF8.GetString(decryptedBytes);
+            return plaintext;
         }
 
     }
